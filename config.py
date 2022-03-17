334cf1bf10b06a866ca40add9c1555bdb3ef788d
ElaineNing <42329802+ElaineNing@users.noreply.github.com>
2022-03-17 12:05:54 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cavyhltr1yt8.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "ej-employee"
+customregion = "us-east-1"
